2c77390811895dca8c1ec47377f30f46aa1c3497
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-04-12 16:25:08 +0200

    Create project

---
+++ gsonExercise/src/main/java/bg/softuni/gsonexercise/GsonExerciseApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.gsonexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsonExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsonExerciseApplication.class, args);
+    }
+
+}
---
+++ gsonExercise/src/test/java/bg/softuni/gsonexercise/GsonExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.gsonexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsonExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
